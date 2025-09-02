e44dce5c4753aba438a69fa3f4d80c1d62de9746
Igor Cibotaru <igorcibotaru13@gmail.com>
2025-09-02 15:12:43 +0300

    Setup React Router and create some components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-
 import './App.css'
-
+import CloudFavicon from './assets/cloud-favicon.png'
+import {Routes, Route} from 'react-router'
+import { HomePage } from './pages/HomePage'
 function App() {
 
   return (
     <>
-      
+      <link rel="shortcut icon" href={CloudFavicon} type="image/x-icon" />
+      <title>Weather App</title>
+      <Routes>
+        <Route index element={<HomePage />}/>
+      </Routes>
     </>
   )
 }
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+import { Header } from "../components/Header";
+import { Background } from "../components/Background";
+export function HomePage () {
+    return (
+        <>
+            <Header />
+            <Background />
+        </>
+    );
+}
